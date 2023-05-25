dca6e938116514aa4ea8fa60ddc66461baba079a
azimust <woody.azimbek@gmail.com>
2023-05-25 22:49:16 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Alphabet from './components/Alphabet/Alphabet';
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='container'>
+        <Alphabet />
+      </div>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Alphabet/Alphabet.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Howl } from 'howler';
+import { alphabet } from '../../data/data';
+import './Alphabet.css'
+
+const Alphabet = () => {
+    const playSound = (src) => {
+        const sound = new Howl({
+            src,
+            html5: true
+        })
+        sound.play();
+    };
+
+    return (
+        <div className='alphabet'>
+        <h1>Алфавит</h1>
+            <div className="alphabet__main">
+                {alphabet.map((el, index) => {
+                    return (
+                        <div onClick={() => playSound(el.sound)} style={{backgroundColor: el.color}} className='alphabet__button' key={index}>
+                            <h3>{el.letter}</h3>
+                            <p>{el.word}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default Alphabet
