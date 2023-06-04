7e31220e7a78257a881d5e9dcc76917f32d2bc92
kyial05 <asuskyial@gmail.com>
2023-06-04 11:41:41 +0500

    countries

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,19 +1,31 @@
 import React from "react";
 import "./Countries.css";
+import { Howl } from "howler";
 import { countries } from "../../data/countries";
 
 const Countries = () => {
+  const playSound = (src) => {
+    const sound = new Howl({
+      src,
+      html5: true,
+    });
+    sound.play();
+  };
   return (
     <div className="country_main">
       <h1>Countries</h1>
       <div className="countries_main">
-        {countries.map((el, index)=>{
+        {countries.map((el, index) => {
           return (
-            <div key={index} className="country_button">
-              <img src={el.img} alt="" className="flag"/>
+            <div
+              key={index}
+              className="country_button"
+              onClick={() => playSound(el.sound)}
+            >
+              <img src={el.img} alt="" className="flag" />
               <p>{el.name}</p>
             </div>
-          )
+          );
         })}
       </div>
     </div>
--- src/data/countries.js
+++ src/data/countries.js
@@ -1,27 +1,194 @@
 import KG from "../components/Countries/countries.img/kyrgyzstan.jpg";
+import kyrgyzstan from "../components/Countries/countries.mp3/kg.mp3";
 import RU from "../components/Countries/countries.img/russia.jpg";
+import russia from "../components/Countries/countries.mp3/russia.mp3";
 import CH from "../components/Countries/countries.img/china.jpg";
+import china from "../components/Countries/countries.mp3/china.mp3";
 import UK from "../components/Countries/countries.img/england.jpg";
+import england from "../components/Countries/countries.mp3/england.mp3";
+import Brazil from "../components/Countries/countries.img/brazil.jpg";
+import brazil from "../components/Countries/countries.mp3/brazil.mp3";
+import Egipt from "../components/Countries/countries.img/egipt.jpg";
+import Ekvador from "../components/Countries/countries.img/ekvador.jpg";
+import France from "../components/Countries/countries.img/france.jpg";
+import India from "../components/Countries/countries.img/india.jpg";
+import Italy from "../components/Countries/countries.img/italy.jpg";
+import Japan from "../components/Countries/countries.img/japan.jpg";
+import Kanada from "../components/Countries/countries.img/kanada.jpg";
+import Kazakhstan from "../components/Countries/countries.img/kazakhstan.jpg";
+import Korea from "../components/Countries/countries.img/korea.jpg";
+import Makedonia from "../components/Countries/countries.img/makedonia.jpg";
+import Mexico from "../components/Countries/countries.img/mexico.jpg";
+import Mongolia from "../components/Countries/countries.img/mongolia.jpg";
+import New_zeland from "../components/Countries/countries.img/new_zeland.jpg";
+import Norvegy from "../components/Countries/countries.img/norvegy.jpg";
+import Oae from "../components/Countries/countries.img/oae.jpg";
+import Polsha from "../components/Countries/countries.img/polsha.jpg";
+import Siria from "../components/Countries/countries.img/siria.jpg";
+import Spain from "../components/Countries/countries.img/spain.jpg";
+import Swiden from "../components/Countries/countries.img/swiden.jpg";
+import Tagikstan from "../components/Countries/countries.img/tagikstan.jpg";
+import Tailand from "../components/Countries/countries.img/tailand.jpg";
+import Taiwan from "../components/Countries/countries.img/taiwan.jpg";
+import Turkey from "../components/Countries/countries.img/turkey.jpg";
+import Ukraina from "../components/Countries/countries.img/ukraina.jpg";
+import Usa from "../components/Countries/countries.img/usa.jpg";
+import Uzbekstan from "../components/Countries/countries.img/uzbekstan.jpg";
 
 export const countries = [
   {
-    name: "Kyrgyzstan",
+    name: "Кыргызстан",
     img: KG,
-    sound: "KG",
+    sound: kyrgyzstan,
   },
   {
-    name: "Russia",
+    name: "Россия",
     img: RU,
-    sound: "RU",
+    sound: russia,
   },
   {
-    name: "China",
+    name: "Китай",
     img: CH,
-    sound: "CH",
+    sound: china,
   },
   {
-    name: "England",
+    name: "Англия",
     img: UK,
-    sound: "UK",
+    sound: england,
+  },
+  {
+    name: "Бразилия",
+    img: Brazil,
+    sound: brazil,
+  },
+  {
+    name: "Египет",
+    img: Egipt,
+    sound: brazil,
+  },
+  {
+    name: "Эквадор",
+    img: Ekvador,
+    sound: brazil,
+  },
+  {
+    name: "Франция",
+    img: France,
+    sound: brazil,
+  },
+  {
+    name: "Индия",
+    img: India,
+    sound: brazil,
+  },
+  {
+    name: "Италия",
+    img: Italy,
+    sound: brazil,
+  },
+  {
+    name: "Япония",
+    img: Japan,
+    sound: brazil,
+  },
+  {
+    name: "Канада",
+    img: Kanada,
+    sound: brazil,
+  },
+  {
+    name: "Казахстан",
+    img: Kazakhstan,
+    sound: brazil,
+  },
+  {
+    name: "Корея",
+    img: Korea,
+    sound: brazil,
+  },
+  {
+    name: "Македония",
+    img: Makedonia,
+    sound: brazil,
+  },
+  {
+    name: "Мексика",
+    img: Mexico,
+    sound: brazil,
+  },
+  {
+    name: "Монголия",
+    img: Mongolia,
+    sound: brazil,
+  },
+  {
+    name: "Новая Зеландия",
+    img: New_zeland,
+    sound: brazil,
+  },
+  {
+    name: "Норвегия",
+    img: Norvegy,
+    sound: brazil,
+  },
+  {
+    name: "ОАЭ",
+    img: Oae,
+    sound: brazil,
+  },
+  {
+    name: "Польша",
+    img: Polsha,
+    sound: brazil,
+  },
+  {
+    name: "Сирия",
+    img: Siria,
+    sound: brazil,
+  },
+  {
+    name: "Испания",
+    img: Spain,
+    sound: brazil,
+  },
+  {
+    name: "Швеция",
+    img: Swiden,
+    sound: brazil,
+  },
+  {
+    name: "Таджикстан",
+    img: Tagikstan,
+    sound: brazil,
+  },
+  {
+    name: "Таиланд",
+    img: Tailand,
+    sound: brazil,
+  },
+  {
+    name: "Тайвань",
+    img: Taiwan,
+    sound: brazil,
+  },
+  {
+    name: "Турция",
+    img: Turkey,
+    sound: brazil,
+  },
+  {
+    name: "Украина",
+    img: Ukraina,
+    sound: brazil,
+  },
+  {
+    name: "США",
+    img: Usa,
+    sound: brazil,
+  },
+  {
+    name: "Узбекстан",
+    img: Uzbekstan,
+    sound: brazil,
   },
 ];
